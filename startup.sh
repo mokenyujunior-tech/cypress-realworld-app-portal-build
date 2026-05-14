ac57bffc340480496fbe7148787f987ea50507e8
Mokenyu <mokenyujunior@gmail.com>
2026-05-14 00:04:35 -0400

    waiting for node_modules extraction before starting backend

--- startup.sh
+++ startup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 cd /home/site/wwwroot
+
+echo "Seeding database..."
 node -e "const fs=require('fs'); fs.copyFileSync('./data/database-seed.json','./data/database.json');"
+
+echo "Waiting for node_modules to be ready..."
+while [ ! -f node_modules/.bin/ts-node ]; do
+  echo "Still waiting..."
+  sleep 2
+done
+
+echo "Starting application..."
 node_modules/.bin/ts-node -P tsconfig.tsnode.json -r tsconfig-paths/register backend/app.ts
