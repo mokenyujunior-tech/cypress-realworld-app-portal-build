06878542561f33238ffc38d532833149bf5f3018
Mokenyu <mokenyujunior@gmail.com>
2026-05-17 02:00:32 -0400

    fix: format fix-prestart.js with prettier

--- scripts/fix-prestart.js
+++ scripts/fix-prestart.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
-const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-pkg.scripts.prestart = "node -e \"const fs=require('fs'); fs.copyFileSync('./data/database-seed.json','./data/database.json');\"";
-fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));
-console.log('prestart script updated successfully');
-console.log('New prestart:', pkg.scripts.prestart);
+const fs = require("fs");
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
+pkg.scripts.prestart =
+  "node -e \"const fs=require('fs'); fs.copyFileSync('./data/database-seed.json','./data/database.json');\"";
+fs.writeFileSync("package.json", JSON.stringify(pkg, null, 2));
+console.log("prestart script updated successfully");
+console.log("New prestart:", pkg.scripts.prestart);
